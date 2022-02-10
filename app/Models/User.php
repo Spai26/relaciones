779afa2ebcc45c3300725e9126999ff1a8090de2
Sergio Ai <sergioai.si93@gmail.com>
2022-02-10 15:02:29 -0500

    Relation User - Profile with models and migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /*
+     * relation 1 to 1 Usuario (User) - Perfil (Profile)
+     !importante Me interesa ver desde el usuario acceder al perfil.
+    */
+    public function profile()
+    {
+        return $this->hasOne(Profile::class);
+    }
 }
