5ea765df9f823bf6b28128a90b328281ade893c2
Sergio Ai <sergioai.si93@gmail.com>
2022-02-11 14:42:23 -0500

    relation model category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    /**
+     * !importante Relation Categoria (Category) - Articulos (Post)
+     *
+     */
+    public function posts(){
+        return $this->hasMany(Post::class);
+    }
+
+      /**
+     * !importante Relation Categoria (Category) - Video (video)
+     *
+     */
+    public function videos(){
+        return $this->hasMany(Video::class);
+    }
 }
