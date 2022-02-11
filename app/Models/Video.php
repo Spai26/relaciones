844418a2f1aa9ad2813fd9258846bdfaeeeeca7d
Sergio Ai <sergioai.si93@gmail.com>
2022-02-11 14:59:14 -0500

    relation video ok

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -8,4 +8,45 @@ use Illuminate\Database\Eloquent\Model;
 class Video extends Model
 {
     use HasFactory;
+
+    /**
+     * ?Post pertenece a ...
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+
+    /**
+     * !importante Relation polimorficas
+     * * 'comentable' se relaciona con la tabla creada en la migracion* -- hasmnay
+     * polimorfica 1 to m
+     * ?Tiene muchos
+     */
+    public function coments()
+    {
+        return $this->morphMany(Comment::class, 'comentable');
+    }
+
+    /**
+     * Relation polimorfica 1 to 1
+     */
+    public function image()
+    {
+        return $this->morphOne(Image::class, 'imageable');
+    }
+
+    /**
+     * Relation polimorfica m to m
+     */
+    public function tags()
+    {
+        return $this->morphToMany(Tag::class, 'taggable');
+    }
 }
